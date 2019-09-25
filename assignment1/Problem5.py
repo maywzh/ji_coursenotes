200be77bc792dba9c7cfd5d4654a69f2c5a19c85
maywzh <maywzh@gmail.com>
2019-09-25 08:14:37 +0800

    finish1

--- assignment1/Problem5.py
+++ assignment1/Problem5.py
@@ -27,7 +27,7 @@ def dectect_key(ciphertext_list):
     for i, cti in enumerate(ciphertext_list):
         counter = collections.Counter()
         for j, ctj in enumerate(ciphertext_list):
-            if i != j:  # Just dont compare with itself 
+            if i != j:  # Just dont compare with itself
                 for k, char in enumerate(str_xor(cti, ctj)):
                     if char in string.printable and char.isalpha():
                         # space(0x20) ^ letter == letter
@@ -41,7 +41,7 @@ def dectect_key(ciphertext_list):
             if val >= 6:
                 possible_space_idxs.append(i)
         # print knownSpaceIndexes # Shows all the positions where we now know the key!
-
+        final_key = [None]*150
         # Now Xor the current_index with spaces, and at the knownSpaceIndexes positions we get the key back!
         space_xor_test = str_xor(cti, ' '*150)
         for i in possible_space_idxs:
@@ -49,16 +49,15 @@ def dectect_key(ciphertext_list):
             final_key[i] = space_xor_test[i]
             # Record that we known the key at this position
             possible_space_idxs.add(i)
+        return final_key, possible_space_idxs
 
 
 def run(target):
-    cyphertext_list, target_ciphertext = read_ciphertexts(
+    ciphertext_list, target_ciphertext = read_ciphertexts(
         "./ciphertext.txt", "./target_ciphertext.txt")
     # To store the final key
-    final_key = [None]*150
     # To store the positions we know are broken
-    known_key_positions = set()
-
+    final_key,possible_space_idxs = dectect_key(ciphertext_list)
     # Construct a hex key from the currently known key, adding in '00' hex chars where we do not know (to make a complete hex string)
     final_key_hex = ''.join(
         [val if val is not None else '00' for val in final_key])
@@ -66,4 +65,7 @@ def run(target):
     output = str_xor(target_ciphertext, final_key_hex)
     # Print the output, printing a * if that character is not known yet
     print(''.join(
-        [char if index in known_key_positions else '*' for index, char in enumerate(output)]))
+        [char if index in possible_space_idxs else '*' for index, char in enumerate(output)]))
+
+if __name__ == "__main__":
+    run()
