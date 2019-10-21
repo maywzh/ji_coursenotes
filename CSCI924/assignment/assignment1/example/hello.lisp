1e3aa096240bda25a281b9f7fd923efd9c676cf3
maywzh <maywzh@gmail.com>
2019-10-21 21:04:43 +0800

    lisp

--- CSCI924/assignment/assignment1/example/hello.lisp
+++ CSCI924/assignment/assignment1/example/hello.lisp
@@ -1,10 +1,5 @@
-
-(defun our-member 
-    (obj lst) 
-    (if 
-        (null lst) nil 
-        (if 
-            (eql 
-                (car lst) obj) lst 
-            (our-member obj 
-                (cdr lst)))))
+(let 
+    (
+        (x 2)
+        (y 1)) 
+    (+ x y) (* x y))
