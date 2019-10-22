162e22ec48dfcd05ca018f0538fbff1be2007190
maywzh <maywzh@gmail.com>
2019-10-22 20:54:08 +0800

    arrange templates

--- CSCI924/assignment/assignment1/example/NumberOfPrimes.lisp
+++ CSCI924/assignment/assignment1/example/NumberOfPrimes.lisp
@@ -1,11 +1,29 @@
+(defun IsPrime 
+	(p)
+	(if 
+		(or 
+			(= p 1) 
+			(= p 2)) 
+		(return 1) 
+		
+		(
+			(loop for i from 2 to 
+				(nth-value 0 
+					(floor 
+						(sqrt lis))) do 
+				(
+					(if 
+						(= 0 
+							(mod p i)) 
+						(return nil)))) 
+			(return 1))))
+
 (defun NumberOfPrimes 
 	(lis)
 	(if 
 		(atom lis) 
 		(if 
-			(not lis) 0 
-			(loop for i from 2 to (nth-value 0 (floor (sqrt lis))) 
-			do  (print i)))
+			(not lis) 0 1)
 		(+ 	
 			(NumberOfPrimes 
 				(car lis)) 
