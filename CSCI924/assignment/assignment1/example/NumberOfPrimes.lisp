035f9ea803af990db44679e0e8d59bb5d18ed99d
maywzh <maywzh@gmail.com>
2019-10-22 16:52:12 +0800

    add lisp code

--- CSCI924/assignment/assignment1/example/NumberOfPrimes.lisp
+++ CSCI924/assignment/assignment1/example/NumberOfPrimes.lisp
@@ -4,9 +4,15 @@
 		(atom lis) 
 		(if 
 			(not lis) 0 
-			(loop (return 1)))
-		(+ 
+			(loop for i from 2 to (nth-value 0 (floor (sqrt lis))) 
+			do  (print i)))
+		(+ 	
 			(NumberOfPrimes 
 				(car lis)) 
 			(NumberOfPrimes 
 				(cdr lis)))))
+
+;(nth-value 1 (floor 4 3))
+;(multiple-value-bind (quot rem)
+;    (floor 4 3)
+;  (format t "The remainder is ~f~%" rem))
