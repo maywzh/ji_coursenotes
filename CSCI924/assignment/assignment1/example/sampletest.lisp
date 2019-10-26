ac8d2ea4307150dabb2102b9a51eadd7ba9ace6a
maywzh <maywzh@gmail.com>
2019-10-26 23:07:30 +0800

    sampletest.lisp

--- CSCI924/assignment/assignment1/example/sampletest.lisp
+++ CSCI924/assignment/assignment1/example/sampletest.lisp
@@ -1,4 +1,6 @@
 (load "question1.lisp")
+(load "question2.lisp")
+
 (print "Question1.a")
 (print (NumberOfPrimes '(((1)(2))(5)(3)((8)3) )))
 
@@ -7,3 +9,5 @@
 
 (print "Question1.c")
 (print (SumIfNot '(1 8 (2)) '(1 (3 (8)) 8 9)))
+
+(print "Question2")
