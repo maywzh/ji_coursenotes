01af4b9e3122953f6b5c3b39deb9be84a2861e70
maywzh <maywzh@gmail.com>
2019-11-04 13:09:48 +0800

    lisp files

--- CSCI924/assignment/assignment1/src/io.lisp
+++ CSCI924/assignment/assignment1/src/io.lisp
@@ -1,6 +1,12 @@
-(let ((in (open "data.txt" :if-does-not-exist nil)))
-  (when in
-    (loop for line = (read-line in nil)
-         while line do (format t "~a~%" line))
-    (close in)))
+
+(let 
+    (
+        (in 
+            (open "data.txt" :if-does-not-exist nil)))
+    (when in
+        (loop for line = 
+            (read-line in nil)
+whilelinedo
+            (format t "~a~%" line))
+        (close in)))
 
