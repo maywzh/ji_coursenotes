a715fa3e71f2cf4d9fe7c3fb0493d7bd20de84b4
maywzh <maywzh@gmail.com>
2020-03-10 05:21:47 +0800

    beautify code

--- 2020spring/CSCI964/assignment1-0316/src/mlp.cpp
+++ 2020spring/CSCI964/assignment1-0316/src/mlp.cpp
@@ -110,126 +110,24 @@ void RandomArray(int *arr, int n, int ordering)
 	}
 }
 
-void TrainNet(float **x, float **d, int NumIPs, int NumOPs, int NumPats)
+void TrainNet(float **x, float **d, int NumIPs, int NumOPs, int NumPats, int Ordering)
 {
-	// Trains 2 layer back propagation neural network
-	// x[][]=>input data, d[][]=>desired output data
-
-	float *h1 = new float[NumHN1];		  // O/Ps of hidden layer
-	float *y = new float[NumOPs];		  // O/P of Net
-	float *ad1 = new float[NumHN1];		  // HN1 back prop errors
-	float *ad2 = new float[NumOPs];		  // O/P back prop errors
-	float PatErr, MinErr, AveErr, MaxErr; // Pattern errors
-	int p, i, j;						  // for loops indexes
-	long ItCnt = 0;						  // Iteration counter
-	long NumErr = 0;					  // Error counter (added for spiral problem)
-
-	cout << "TrainNet2: IP:" << NumIPs << " H1:" << NumHN1 << " OP:" << NumOPs << endl;
-
-	// Allocate memory for weights
-	w1 = Aloc2DAry(NumIPs, NumHN1); // 1st layer wts
-	w11 = Aloc2DAry(NumIPs, NumHN1);
-	w111 = Aloc2DAry(NumIPs, NumHN1);
-	w2 = Aloc2DAry(NumHN1, NumOPs); // 2nd layer wts
-	w22 = Aloc2DAry(NumHN1, NumOPs);
-	w222 = Aloc2DAry(NumHN1, NumOPs);
-
-	// Init wts between -0.5 and +0.5
-	srand(time(0));
-	for (i = 0; i < NumIPs; i++)
-		for (j = 0; j < NumHN1; j++)
-			w1[i][j] = w11[i][j] = w111[i][j] = float(rand()) / RAND_MAX - 0.5;
-	for (i = 0; i < NumHN1; i++)
-		for (j = 0; j < NumOPs; j++)
-			w2[i][j] = w22[i][j] = w222[i][j] = float(rand()) / RAND_MAX - 0.5;
-
-	for (;;)
-	{ // Main learning loop
-		MinErr = 3.4e38;
-		AveErr = 0;
-		MaxErr = -3.4e38;
-		NumErr = 0;
-
-		for (p = 0; p < NumPats; p++)
-		{ // for each pattern...
-			// Cal neural network output
-			for (i = 0; i < NumHN1; i++)
-			{ // Cal O/P of hidden layer 1
-				float in = 0;
-				for (j = 0; j < NumIPs; j++)
-					in += w1[j][i] * x[p][j];
-				h1[i] = (float)(1.0 / (1.0 + exp(double(-in)))); // Sigmoid fn
-			}
-			for (i = 0; i < NumOPs; i++)
-			{ // Cal O/P of output layer
-				float in = 0;
-				for (j = 0; j < NumHN1; j++)
-				{
-					in += w2[j][i] * h1[j];
-				}
-				y[i] = (float)(1.0 / (1.0 + exp(double(-in)))); // Sigmoid fn
-			}
-			// Cal error for this pattern
-			PatErr = 0.0;
-			for (i = 0; i < NumOPs; i++)
-			{
-				float err = y[i] - d[p][i]; // actual-desired O/P
-				if (err > 0)
-					PatErr += err;
-				else
-					PatErr -= err;
-				NumErr += ((y[i] < 0.5 && d[p][i] >= 0.5) || (y[i] >= 0.5 && d[p][i] < 0.5)); //added for binary classification problem
-			}
-			if (PatErr < MinErr)
-				MinErr = PatErr;
-			if (PatErr > MaxErr)
-				MaxErr = PatErr;
-			AveErr += PatErr;
-
-			// Learn pattern with back propagation
-			for (i = 0; i < NumOPs; i++)
-			{ // Modify layer 2 wts
-				ad2[i] = (d[p][i] - y[i]) * y[i] * (1.0 - y[i]);
-				for (j = 0; j < NumHN1; j++)
-				{
-					w2[j][i] += LrnRate * h1[j] * ad2[i] +
-								Mtm1 * (w2[j][i] - w22[j][i]) +
-								Mtm2 * (w22[j][i] - w222[j][i]);
-					w222[j][i] = w22[j][i];
-					w22[j][i] = w2[j][i];
-				}
-			}
-			for (i = 0; i < NumHN1; i++)
-			{ // Modify layer 1 wts
-				float err = 0.0;
-				for (j = 0; j < NumOPs; j++)
-					err += ad2[j] * w2[i][j];
-				ad1[i] = err * h1[i] * (1.0 - h1[i]);
-				for (j = 0; j < NumIPs; j++)
-				{
-					w1[j][i] += LrnRate * x[p][j] * ad1[i] +
-								Mtm1 * (w1[j][i] - w11[j][i]) +
-								Mtm2 * (w11[j][i] - w111[j][i]);
-					w111[j][i] = w11[j][i];
-					w11[j][i] = w1[j][i];
-				}
-			}
-		} // end for each pattern
-		ItCnt++;
-		AveErr /= NumPats;
-		float PcntErr = NumErr / float(NumPats) * 100.0;
-		cout << "Test: " << endl;
-		cout.setf(ios::fixed | ios::showpoint);
-		cout << setprecision(6) << setw(6) << ItCnt << ": " << setw(12) << MinErr << setw(12) << AveErr << setw(12) << MaxErr << setw(12) << PcntErr << endl;
-
-		if ((AveErr <= ObjErr) || (ItCnt == NumIts))
-			break;
-	} // end main learning loop
-	// Free memory
-	delete[] h1;
-	delete[] y;
-	delete[] ad1;
-	delete[] ad2;
+	if (NumHN == 1)
+	{
+		TrainNet2(x, d, NumIPs, NumOPs, NumPats, Ordering);
+	}
+	else if (NumHN == 2)
+	{
+		TrainNet3(x, d, NumIPs, NumOPs, NumPats, Ordering);
+	}
+	else if (NumHN == 3)
+	{
+		TrainNet4(x, d, NumIPs, NumOPs, NumPats, Ordering);
+	}
+	else
+	{
+		cout << "Exceed hidden layer limit" << endl;
+	}
 }
 
 // Trains 2 layer back propagation neural network
@@ -1096,25 +994,9 @@ int main()
 			fin >> OPTstData[i][j];
 	}
 	fin.close();
-	if (NumHN == 1)
-	{
-		TrainNet2(IPTrnData, OPTrnData, NumIPs, NumOPs, NumTrnPats, Ordering);
-		TestNet2(IPTstData, OPTstData, NumIPs, NumOPs, NumTstPats);
-	}
-	else if (NumHN == 2)
-	{
-		TrainNet3(IPTrnData, OPTrnData, NumIPs, NumOPs, NumTrnPats, Ordering);
-		TestNet3(IPTstData, OPTstData, NumIPs, NumOPs, NumTstPats);
-	}
-	else if (NumHN == 3)
-	{
-		TrainNet4(IPTrnData, OPTrnData, NumIPs, NumOPs, NumTrnPats, Ordering);
-		TestNet4(IPTstData, OPTstData, NumIPs, NumOPs, NumTstPats);
-	}
-	else
-	{
-		cout << "Exceed hidden layer limit" << endl;
-	}
+	TrainNet(IPTrnData, OPTrnData, NumIPs, NumOPs, NumTrnPats, Ordering);
+	TestNet(IPTstData, OPTstData, NumIPs, NumOPs, NumTstPats);
+
 	Free2DAry(IPTrnData, NumTrnPats);
 	Free2DAry(OPTrnData, NumTrnPats);
 	Free2DAry(IPTstData, NumTstPats);
