8dadc4167695e48b1b3542febcd6f2157dce90a0
maywzh <maywzh@gmail.com>
2020-03-11 02:04:31 +0800

    pandas init

--- 2020spring/CSCI964/lab4-0317/src/lab4.py
+++ 2020spring/CSCI964/lab4-0317/src/lab4.py
@@ -4,26 +4,12 @@ import random
 import libsvm.commonutil as commonutil
 import libsvm.svmutil as svmutil
 
-# def readData(filepath, ifShuffle, feature_num=4):
-#     label_dict = {}
-#     xs = []
-#     ys = []
-#     ynums = []
-#     label_num = 0
-#     with open(filepath) as f:
-#         lines = f.readlines()
-#         if ifShuffle:
-#             random.shuffle(lines)
-#         for line in lines:
-#             x = list(map(float, line.split(',')[0:feature_num]))
-#             xs.append(np.array(x).reshape(feature_num, 1))
-#             label_str = line.strip('\n').split(',')[-1]
-#             if not label_str in list(label_dict.keys()):
-#                 label_num += 1
-#                 label_dict[label_str] = label_num
-#             ynums.append(label_dict[label_str])
-#         for ynum in ynums:
-#             y = np.zeros(label_num)
-#             y[ynum - 1] = 1.0
-#             ys.append(y.reshape(label_num, 1))
-#     return xs, ys
+from sklearn.datasets import dump_svmlight_file
+df = pd.DataFrame([['green', 'M', 10.1, 'class1'], ['red', 'L', 13.5, 'class2'], ['blue', 'XL', 15.3, 'class1']])
+df.columns = ['color', 'size', 'prize', 'label']
+size_mapping = {'XL': 3, 'L': 2, 'M': 1}
+df['size'] = df['size'].map(size_mapping)
+class_mapping = {label: idx for idx, label in enumerate(set(df['label']))}
+df['label'] = df['label'].map(class_mapping)
+y = df.label  # y为数据的label值
+dummy = pd.get_dummies(df.iloc[:, :-1])
