8e5eb0f83a2e9af99d3cf0561e62dcfa579a7523
maywzh <maywzh@gmail.com>
2020-05-29 18:39:19 +0800

    introduction

--- 2020spring/CSCI933/assignment2-0421/src/mlp/model.py
+++ 2020spring/CSCI933/assignment2-0421/src/mlp/model.py
@@ -14,10 +14,15 @@ numpy.random.seed(seed)
 # url = "http://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data"
 # dataset = numpy.loadtxt("pima-indians-diabetes.data", delimiter=",")
 
+names = [
+    'no_pregnant', 'pla_glu_concentration', 'blood_pressure(mmHg)',
+    'skin_fold_thickness(mm)', 'serum_insulin(U/ml)', 'bmi',
+    'diab_pedigree_fun', 'age', 'class'
+]
 
-names = ['no_pregnant', 'pla_glu_concentration', 'blood_pressure(mmHg)', 'skin_fold_thickness(mm)', 'serum_insulin(U/ml)', 'bmi', 'diab_pedigree_fun', 'age', 'class']
-
-dataset = pandas.read_csv('pima-indians-diabetes.data.csv', delimiter=',', names=names)
+dataset = pandas.read_csv('pima-indians-diabetes.data.csv',
+                          delimiter=',',
+                          names=names)
 
 arrays = dataset.values
 
@@ -26,13 +31,12 @@ arrays = dataset.values
 # print dataset.describe()
 # print dataset.groupby('no_pregnant').size()
 
-X=arrays[:,0:8]
-Y=arrays[:,8]
+X = arrays[:, 0:8]
+Y = arrays[:, 8]
 
 validation_size = 0.25
-X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
-
-
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(
+    X, Y, test_size=validation_size, random_state=seed)
 
 
 #Create model
@@ -42,13 +46,22 @@ model.add(Dense(16, init='uniform', activation='relu'))
 model.add(Dense(1, init='uniform', activation='sigmoid'))
 
 # Compile model
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
 
 # Set log directory for tensorboard
-tensorboard = TensorBoard(log_dir="logs", histogram_freq=0, write_graph=True, write_images=True)
+tensorboard = TensorBoard(log_dir="logs",
+                          histogram_freq=0,
+                          write_graph=True,
+                          write_images=True)
 
 # Fit the model
-model.fit(X_train, Y_train, nb_epoch=10000, batch_size=40, callbacks=[tensorboard])
+model.fit(X_train,
+          Y_train,
+          nb_epoch=10000,
+          batch_size=40,
+          callbacks=[tensorboard])
 
 # scores = model.evaluate(X_validation,Y_validation)
 # print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
@@ -60,4 +73,4 @@ with open("model.json", "w") as json_file:
 
 # Serialize weights to HDF5
 model.save_weights("model.h5")
-print ("Saved model")
+print("Saved model")
