b9cf8ea3441e5b5bf075925f44fd7cd208875e93
maywzh <maywzh@gmail.com>
2020-06-20 19:05:44 +0800

    solution5

--- 2020summer/CSCI835/assignment/assignment0/solution5.sql
+++ 2020summer/CSCI835/assignment/assignment0/solution5.sql
@@ -6,9 +6,24 @@ SET LINESIZE 100
 SET PAGESIZE 200 
 SET SERVEROUTPUT ON
 
+SELECT DISTINCT t.fname, t.lname
+FROM traveller t 
+    INNER JOIN visit v ON t.pnum = v.pnum AND t.nationality = v.nationality 
+    INNER JOIN hotel h ON v.hname = h.hname and v.hcity = h.city
+WHERE h.star = 4;
 
+SELECT hname, (SELECT COUNT(*) FROM visit v WHERE v.hname = h.hname AND v.hcity = h.city) as visitors_num
+FROM hotel h;
 
-COMMIT;
+SELECT hname as visitors_num
+FROM hotel h
+WHERE (SELECT COUNT(*) FROM visit v WHERE v.hname = h.hname AND v.hcity = h.city) = 0;
+
+SELECT lname from traveller 
+UNION
+SELECT hname from hotel;
+
+--COMMIT;
 
 PROMPT solution5.sql done.
 spool off
