263fcbbb8361e2a4ab6da9fe5cfa537ebe0716f2
maywzh <maywzh@gmail.com>
2020-06-20 22:01:15 +0800

    sql4

--- 2020summer/CSCI835/assignment/assignment0/solution4.sql
+++ 2020summer/CSCI835/assignment/assignment0/solution4.sql
@@ -21,7 +21,7 @@ UPDATE visit v
 SET arrival_date = TO_DATE('01-JUN-2019', 'DD-MON-YYYY')
 WHERE pnum='152483045685' AND nationality = 'China' AND hname = 'Kyono Mori Yurinsha' AND hcity = 'Kyoto' AND arrival_date = TO_DATE('3-JUNE-2019', 'DD-MON-YYYY');
 
---COMMIT;
+COMMIT;
 
 PROMPT solution4.sql done.
 spool off
