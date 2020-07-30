072c41d74b49b807e1cc6a849d275f7c6a0ffc1a
maywzh <maywzh@gmail.com>
2020-07-30 21:15:31 +0800

    solution1

--- 2020summer/CSCI835/final/src/solution3.sql
+++ 2020summer/CSCI835/final/src/solution3.sql
@@ -5,19 +5,6 @@ set linesize 100
 set pagesize 200
 SET SERVEROUTPUT ON
 
--- drop the trigger if it exists
-DECLARE 
-  l_count INTEGER;
-BEGIN
-  SELECT COUNT(*) INTO l_count
-  FROM user_triggers
-  WHERE TRIGGER_NAME = 'MODIFYEMPLOYEEDEPT';
-
-  IF l_count > 0 THEN
-     EXECUTE IMMEDIATE 'DROP TRIGGER ModifyEmployeeDept';
-  END IF;
-END;
-/
 
 -- Create PL/SQL procedure
 CREATE OR REPLACE PROCEDURE INSERT_NEW_EMPLOYEE(E#_ IN Employee.E#%TYPE, Name_ IN Employee.Name%TYPE, DOB_ IN Employee.DOB%TYPE, Supervisor#_ IN Employee.Supervisor#%TYPE, DName_ IN Employee.DName%TYPE) IS tne_ Department.TNE%TYPE;
@@ -29,8 +16,5 @@ BEGIN
 END INSERT_NEW_EMPLOYEE;
 /
 
-SELECT TNE FROM Department WHERE DName='Product Development Department';
-EXECUTE INSERT_NEW_EMPLOYEE('RX590', 'Linda A. Hughes', TO_DATE('12-12-2013', 'DD-MM-YYYY'), 'AE798', 'Product Development Department');
-SELECT TNE FROM Department WHERE DName='Product Development Department';
 
 spool off
