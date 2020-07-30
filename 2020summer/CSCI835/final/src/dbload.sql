4be49b40dab42053167ed29aae4dc3655fd895ba
maywzh <maywzh@gmail.com>
2020-07-30 15:23:50 +0800

    solution3

---
+++ 2020summer/CSCI835/final/src/dbload.sql
@@ -0,0 +1,3 @@
+SELECT TNE FROM Department WHERE DName='Product Development Department';
+EXECUTE INSERT_NEW_EMPLOYEE('RX590', 'Linda A. Hughes', TO_DATE('12-12-2013', 'DD-MM-YYYY'), 'AE798', 'Product Development Department');
+SELECT TNE FROM Department WHERE DName='Product Development Department';
