6a8906036d319dd3c8ba01951b179e7a087faae2
maywzh <maywzh@M-iMac-Pro.423-Net>
2020-10-30 17:51:20 +0800

    cmake

--- 2020fall/CSCI851/assignment/assignment1/code/main.cpp
+++ 2020fall/CSCI851/assignment/assignment1/code/main.cpp
@@ -3,27 +3,18 @@
 
 int main(int argc, char* argv[]) {
 
-    read(argv[2],readAnimal);
-    read(argv[3],readVet);
-    read(argv[4],readProblems);
-    read(argv[5],readTreatments);
-
-    cout<<"Animal"<<endl;
+    read(argv[2], readAnimal);
+    read(argv[3], readVet);
+    read(argv[4], readProblems);
+    read(argv[5], readTreatments);
+    cout << "Animal" << endl;
     printAnimal();
-    cout<<"\nVets"<<endl;
+    cout << "\nVets" << endl;
     printVet();
-    cout<<"\nProblems"<<endl;
+    cout << "\nProblems" << endl;
     printProblems();
-    cout<<"\nTreatments"<<endl;
+    cout << "\nTreatments" << endl;
     printTreatments();
-
-
-//    cout<<"ARGV CONTENTS"<<endl;
-//    for(int x=2;x<argc;x++){
-//        cout<<argv[x]<<endl;
-//
-//    }
-
 
     return 0;
 }
