c5ef10522319fd29fb6a4b83d9822c203a4f7b91
maywzh <maywzh@gmail.com>
2021-01-25 02:13:08 +0800

    fix

--- 2020fall/CSCI991/thesis/compile-thesis.sh
+++ 2020fall/CSCI991/thesis/compile-thesis.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+###
+ # @Author       : maywzh
+ # @Date         : 2020-12-05 00:00:45
+ # @LastEditTime : 2021-01-25 01:18:59
+ # @LastEditors  : maywzh
+ # @Description  : 
+ # @FilePath     : /ji_coursenotes/2020fall/CSCI991/thesis/compile-thesis.sh
+ # @
+ # Copyright (c) 2017 maywzh.
+ # 
+ # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ # 
+ # The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ # 
+ # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+### 
 # A script to compile the PhD Thesis - Krishna Kumar 
 # Distributed under GPLv2.0 License
 
@@ -25,6 +41,7 @@ if [ $1 = $clean ]; then
 	rm -rf *.out*
 	rm -rf *.nlo
 	rm -rf *.nls
+	rm -rf *.synctex*
 	rm -rf $filename.pdf
 	rm -rf $filename.ps
 	rm -rf $filename.dvi
